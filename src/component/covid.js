19393da26bdf4cdd883395b10950a397d9e86ab6
Javeriya Saeed Abbasi <jiyasaeed333@gmail.com>
2022-06-28 08:49:44 +0500

    province data

--- src/component/covid.js
+++ src/component/covid.js
@@ -9,15 +9,58 @@ import Main_card from "./main_card";
 
 const Covid = () =>{
   const [data, setData] = useState([]);
-  
+  const [balochistanData, setBalochistanData] = useState ([]);
+  const [sindhData, setSindhData] = useState ([]);
+  const [punjabData, setPunjabData] = useState ([]);
+  const [kpkData, setKpkData] = useState ([]);
+  const [gilgitData, setGilgitData] = useState ([]);
+  const [kashmirData, setKashmirData] = useState ([]);
+  const [islmabadData, setIslmabadData] = useState ([]);
+
 
    const getCovidData =async () => {    
     try{
         const response = await fetch('https://api.covid19api.com/live/country/pakistan'); 
         const actualData = await response.json();
 
+
         console.log(actualData);
-        console.log(actualData[0]);
+        // Balochistan Data;
+        let balochistanData = actualData[0];
+        console.log(balochistanData);
+        setBalochistanData(balochistanData);
+
+        // Sindh Data;
+        let sindhData = actualData[6];
+        console.log(sindhData);
+        setSindhData(sindhData);
+
+        
+        // Punjab Data;
+        let punjabData = actualData[4];
+        console.log(punjabData);
+        setPunjabData(punjabData);
+
+          
+        // Kpk Data;
+        let kpkData = actualData[2];
+        console.log(kpkData);
+        setKpkData(kpkData);
+
+        // Gilgit Data;
+        let gilgitData = actualData[1];
+        console.log(gilgitData);
+        setGilgitData(gilgitData);
+
+         // Kashmir Data;
+        let kashmirData = actualData[3];
+        console.log(kashmirData);
+        setKashmirData(kashmirData);
+
+        // Islamabad Data;
+        let islmabadData = actualData[3];
+        console.log(islmabadData);
+        setIslmabadData(islmabadData);
     }
     catch (error){
         console.log(error);
@@ -35,6 +78,62 @@ const Covid = () =>{
      setData(
       [province]
      )
+   }, []);
+
+   const [allCountries, setAllCountries] = useState([]);
+   const [totalPak, setTotalPak] = useState([]);
+   const [totalConfirmed, setTotalConfirmed] = useState([]);
+   const [totalDeaths, setTotalDeaths] = useState([]);
+   const [totalRecovered, setTotalRecovered] = useState([]);
+  //  New Cases;
+  const [newConfirmed, setNewConfirmed] = useState([]);
+  const [newDeaths, setNewDeaths] = useState([]);
+  const [newRecovered, setNewRecovered] = useState([]);
+
+   const getTotalToday  =async () => {    
+    try{
+        const response = await fetch('https://api.covid19api.com/summary'); 
+        const actualData1 = await response.json();
+
+        let allCountries = actualData1;
+        console.log(actualData1);
+
+        let totalPak = allCountries.Countries[131]; 
+        console.log(actualData1.Countries[131]);
+
+        let totalConfirmed = totalPak.TotalConfirmed;
+        console.log("Today Total Confirmed Cases PK " + totalConfirmed);
+        setTotalConfirmed(totalConfirmed);
+
+        let totalRecovered = totalPak.TotalRecovered;
+        console.log("Today Total Deaths Cases PK " + totalRecovered);
+        setTotalRecovered(totalRecovered);
+
+        let totalDeaths = totalPak.TotalDeaths;
+        console.log("Today Total Deaths Cases PK " + totalDeaths);
+        setTotalDeaths(totalDeaths);
+
+        // New Cases in Pakistan;
+
+        let newConfirmed = totalPak.NewConfirmed;
+        console.log("Today New Confirmed Cases PK " + newConfirmed);
+        setNewConfirmed(newConfirmed);
+
+        let newRecovered = totalPak.NewRecovered;
+        console.log("Today New Deaths Cases PK " + newRecovered);
+        setNewRecovered(newRecovered);
+
+        let newDeaths = totalPak.NewDeaths;
+        console.log("Today New Deaths Cases PK " + newDeaths);
+        setNewDeaths(newDeaths);
+
+    }
+    catch (error){
+        console.log(error);
+    }
+   }
+   useEffect(() => {
+    getTotalToday ();
    }, []);
    
   //  const  getTotalCases =async () =>{
@@ -132,13 +231,17 @@ const Covid = () =>{
                 <div className="img2 d-flex justify-content-center">
                   <img src={img2} alt="prevent"/>
                 </div>
-                <Main_card/>
-                {/* <div className="row">
+           {/* Pakistan Todays Total Data */}
+           <div className="wrapper1 my-4">
+           <div className="text-center">
+                  <h2>Pakistan Data</h2>
+                </div>
+                <div className="row">
                 <div className="card1 col-lg-4">
                   <div className="card">
                     <div className="card-body">
-                      <h5 className="card-title">Total Cases</h5>
-                      <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                      <h5 className="card-title">Total Confirmed</h5>
+                      <h6 className="card-subtitle mb-2 text-muted">{totalConfirmed}</h6>
                       <p className="card-text"></p>
                     </div>
                   </div>
@@ -147,8 +250,8 @@ const Covid = () =>{
                   <div className="card">
                   <div className="card-body">
                     <h5 className="card-title">Recovered Cases</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h6 className="card-subtitle mb-2 text-muted">{totalRecovered}</h6>
+                    <p className="card-text"></p>
                   </div>
                 </div>
                 </div>
@@ -156,13 +259,86 @@ const Covid = () =>{
                   <div className="card">
                   <div className="card-body">
                     <h5 className="card-title">Death Cases</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h6 className="card-subtitle mb-2 text-muted">{totalDeaths}</h6>
+                    <p className="card-text"></p>
                   </div>
                 </div>
                 </div>
-                </div> */}
                 </div>
+           </div>
+           <div className="wrapper2 my-4">
+           <div className="text-center">
+                  <h2>New Cases</h2>
+                </div>
+                <div className="row">
+                <div className="card1 col-lg-4">
+                  <div className="card">
+                    <div className="card-body">
+                      <h5 className="card-title">Total Confirmed</h5>
+                      <h6 className="card-subtitle mb-2 text-muted">{newConfirmed}</h6>
+                      <p className="card-text"></p>
+                    </div>
+                  </div>
+                </div>
+                <div className="card2 col-lg-4">
+                  <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">Recovered Cases</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">{newRecovered}</h6>
+                    <p className="card-text"></p>
+                  </div>
+                </div>
+                </div>
+                <div className="card3 col-lg-4">
+                  <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">Death Cases</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">{newDeaths}</h6>
+                    <p className="card-text"></p>
+                  </div>
+                </div>
+                </div>
+                </div>
+           </div>
+           <div className="row row1 my-4">
+          <div className="col-lg-4">
+              {/* Province wise Data */}
+              <div className="balochistan">
+                <Main_card data={balochistanData}/>
+                </div>
+          </div>
+          <div className="col-lg-4">
+          <div className="sindh">
+                  <Main_card data={sindhData}/>
+                </div>  
+          </div>
+          <div className="col-lg-4">
+          <div className="sindh">
+                  <Main_card data={kpkData}/>
+                </div>  
+          </div>
+          </div>
+          <div className="row row2">
+          <div className="col-lg-4">
+          <div className="sindh">
+                  <Main_card data={punjabData}/>
+                </div>  
+          </div>
+          <div className="col-lg-4">
+          <div className="sindh">
+                  <Main_card data={gilgitData}/>
+                </div>  
+          </div>
+          <div className="col-lg-4">
+          <div className="sindh">
+                  <Main_card data={kashmirData}/>
+                </div>  
+          </div>
+
+           </div>
+              
+               
+         </div>
               
              
               </div>
--- src/component/main_card.js
+++ src/component/main_card.js
@@ -6,19 +6,24 @@ import img1 from '../images/floating-mask.png';
 import img2 from '../images/preventions.jpg';
 import covid from './covid.css';
 
- const main_card = () => {
+ const main_card = ({data}) => {
+  // console.log("bal", data);
+  // console.log("sindh", data);
+  // console.log("bal", data);
+  console.log("sindh", data);
+
   return (
-    <div className="card">
+    <div className="card mb-3">
     <div className="card-body">
-      <h5 className="card-title">Balochistan</h5>
-      <h6 className="card-subtitle mb-2 text-muted">Total Cases</h6>
-      <p className="card-text">2333,4344</p>
+      <h5 className="card-title">{data.Province}</h5>
+      <h6 className="card-subtitle mb-2 text-muted">Recovered Cases</h6>
+      <p className="card-text">{data.Recovered}</p>
       <h6 className="card-subtitle mb-2 text-muted">Confirmed Cases</h6>
-      <p className="card-text">26893</p>
+      <p className="card-text">{data.Confirmed}</p>
       <h6 className="card-subtitle mb-2 text-muted">Deaths</h6>
-      <p className="card-text">304</p>
+      <p className="card-text">{data.Deaths}</p>
       <h6 className="card-subtitle mb-2 text-muted">Active Cases</h6>
-      <p className="card-text">793</p>
+      <p className="card-text">{data.Active}</p>
     </div>
   </div>
   )
